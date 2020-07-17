7691cac89307fb60614e23f608075af228a3797f
Nicholas Klaene <nicholasklaene@gmail.com>
2020-07-17 17:18:17 -0400

    removed animation from style.css - it is now only in animation.css

---
+++ script.js
@@ -0,0 +1,7 @@
+const name = document.querySelector('#name');
+const email = document.querySelector('#email');
+const company = document.querySelector('#company');
+const message = document.querySelector('#message');
+const form = document.querySelector('form');
+
+console.log(name,email,company,message, form);
